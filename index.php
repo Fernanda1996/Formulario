fc184192365deee089280bbba1c3048cb83e9905
Fernanda1996 <52184870+Fernanda1996@users.noreply.github.com>
2019-06-25 20:56:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,27 @@
   <title>Trabajo en clase</title>
 </head>
 <body>
+ 
+
+<table>
+<tr>
+  <td><strong>Nombre</strong></td>
+  <td><strong>Apellido</strong></td>
+  <td><strong>Direccion</strong></td>
+</tr>
+
+<tr>
+  <td>Fernanda</td>
+  <td>Umajinga</td>
+  <td>Guamani Alto</td>
+</tr>
+
+<tr>
+  <td>Henry</td>
+  <td>Yugsi</td>
+  <td>Chillogallo</td>
+</tr>
+
+</table>
 </body>
 </html>
